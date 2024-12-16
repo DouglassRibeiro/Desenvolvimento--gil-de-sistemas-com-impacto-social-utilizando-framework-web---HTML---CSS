8d065df4deb39b4a2f2cd7a628cb1192b728ab0b
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2024-12-15 22:07:55 -0300

    Trilha 4: HTML e CSS - Prático - #08 - Desenvolvimento Modais Dinâmicos - Parte 2

---
+++ assets/js/popup.js
@@ -0,0 +1,15 @@
+document.getElementById("openModelCompra").addEventListener("click", function(){
+    document.getElementById("modalCompra").style.display ="grid";
+})
+
+document.getElementById("closedModelBtnCompra").addEventListener("click", function(){
+    document.getElementById("modalCompra").style.display ="none";
+})
+
+document.getElementById("openModelVenda").addEventListener("click", function(){
+    document.getElementById("modalVenda").style.display ="grid";
+})
+
+document.getElementById("closedModelBtnVenda").addEventListener("click", function(){
+    document.getElementById("modalVenda").style.display ="none";
+})
